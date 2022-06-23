69968f12c023e4ab139a9be8a0b171c99b5b2c8c
Bigkidhj <j2261305@gmail.com>
2022-06-23 16:28:37 +0900

    코루틴 적용

--- MobileDefenseGame/Assets/Scripts/BulletBehavior.cs
+++ MobileDefenseGame/Assets/Scripts/BulletBehavior.cs
@@ -7,7 +7,6 @@ public class BulletBehavior : MonoBehaviour {
     public BulletStat bulletStat { get; set; }
 
     public float activeTime = 3.0f;
-    public float spawnTime;
 
     public BulletBehavior() //생성자, 변수의 초기화 >> Start, Update 함수보다 먼저 실행되어야 한다.
     {
@@ -19,22 +18,21 @@ public class BulletBehavior : MonoBehaviour {
     public void Spawn()
     {
         gameObject.SetActive(true);
-        spawnTime = Time.time;
     }
 
-	void Start () {
-        Spawn();
-	}
+    private void OnEnable() // 오브젝트가 활성화 되면
+    {
+        StartCoroutine(BulletInactive(activeTime));
+    }
+
+    IEnumerator BulletInactive(float activeTime)
+    {
+        yield return new WaitForSeconds(activeTime);
+        gameObject.SetActive(false);
+    }
 	
 	void Update () {
-        if(Time.time - spawnTime >= activeTime)
-        {
-            gameObject.SetActive(false);
-        }
-        else
-        {
-            transform.Translate(Vector2.right * bulletStat.speed * Time.deltaTime);
-        }       
+            transform.Translate(Vector2.right * bulletStat.speed * Time.deltaTime);     
 	}
 
     private void OnTriggerEnter2D(Collider2D other)
--- MobileDefenseGame/Assets/Scripts/CreateMonster.cs
+++ MobileDefenseGame/Assets/Scripts/CreateMonster.cs
@@ -11,50 +11,61 @@ public class CreateMonster : MonoBehaviour {
 
     private GameObject monsterPrefab;
 
-    private float lastSpawnTime;
     private int spawnCount = 0;
+    private IEnumerator coroutine; // coroutine 변수 선언
 
     void Start () {
         
         monsterPrefab = monster1Prefab;
-        lastSpawnTime = Time.time;
+        coroutine = process(); // coroutine란 변수를 process 코루틴으로 지정
+        StartCoroutine(coroutine);
 	}
-	
-	void Update () {
-		if(GameManager.instance.round <= GameManager.instance.totalRound)
+
+    void Create()
+    {
+        int index = Random.Range(0, 4);
+        GameObject respawnSpot = respawnSpotList[index];
+        Instantiate(monsterPrefab, respawnSpot.transform.position, Quaternion.identity);
+        GameManager.instance.monsterAddCount++;
+        spawnCount += 1;
+    }
+
+    IEnumerator process()
+    {
+        while (true)
         {
-            float timeGap = Time.time - lastSpawnTime;
-            if(((spawnCount == 0 && timeGap > GameManager.instance.roundReadyTime) // 새 라운드가 시작
-                || timeGap > GameManager.instance.spawnTime)
-                && spawnCount < GameManager.instance.spawnNumber)
+            if (GameManager.instance.round > GameManager.instance.totalRound) StopCoroutine(coroutine);
+            if(spawnCount < GameManager.instance.spawnNumber) // 몬스터의 출몰 한계
             {
-                lastSpawnTime = Time.time;
-                int index = Random.Range(0, 4);
-                GameObject respawnSpot = respawnSpotList[index];
-                Instantiate(monsterPrefab, respawnSpot.transform.position, Quaternion.identity);
-                GameManager.instance.monsterAddCount++;
-                spawnCount += 1;
+                Create();
             }
             if(spawnCount == GameManager.instance.spawnNumber &&
-               GameObject.FindGameObjectWithTag("Monster") == null)
+                GameObject.FindGameObjectWithTag("Monster") == null) 
+                // 소환된 몬스터 수가 클리어 목표 몬스터 수와 같고, 현재 몬스터가 존재하지 않는다면
             {
                 if(GameManager.instance.totalRound == GameManager.instance.round)
+                //현재 스테이지가 전체 중 마지막 스테이지라면
                 {
-                    GameManager.instance.gameClear();
-                    GameManager.instance.round += 1;
-                    return;
+                    GameManager.instance.gameClear(); // 게임 클리어
+                    GameManager.instance.round += 1; // 코루틴 실행을 위한 라운드 증가
                 }
-                GameManager.instance.clearRound();
-                spawnCount = 0;
-                lastSpawnTime = Time.time;
-
-                if(GameManager.instance.round == 4)
+                else
                 {
-                    monsterPrefab = monster2Prefab;
-                    GameManager.instance.spawnTime = 2.0f;
-                    GameManager.instance.spawnNumber = 10;
+                    GameManager.instance.clearRound();
+                    spawnCount = 0;
+
+                    if (GameManager.instance.round == 4)
+                    {
+                        monsterPrefab = monster2Prefab;
+                        GameManager.instance.spawnTime = 2.0f;
+                        GameManager.instance.spawnNumber = 10;
+                    }
                 }
             }
+            if(spawnCount == 0) yield return new WaitForSeconds(GameManager.instance.roundReadyTime);
+            // 새 라운드가 시작됐다면 라운드 준비 시간 만큼 잠시 대기
+            else yield return new WaitForSeconds(GameManager.instance.spawnTime);
+            // 그 이후부터는 몬스터 소환 쿨타임만큼 대기
         }
-	}
+    }
 }
